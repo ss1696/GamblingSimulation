0cd48dbc646bd93bb2c9848c2d2a82989f033a27
shashank srivastava <shashank.1696@gmail.com>
2020-07-30 21:08:49 +0530

    Added: Invoked Methods to calculate the stake till the gambler either looses or wins the 50% of the stake

--- Gambling.java
+++ Gambling.java
@@ -1,27 +1,58 @@
 public class Gambling{
 	
 	// Initializing the Varible ...
-	static int Stake = 100;
+	static int stake = 100;
 	final static int Bet = 1;
 	static int numberOfGames=0;
 	
 	public static void main(String[] args){
-		Gambling gambler = new Gambling();
-		gambler.winOrLoss();
+		display();
+		playGame();
 	}
-
-	//This function return true(Win) when probability is less then 0.5 else return false(loss) and update the Stake with bet value
-	static boolean winOrLoss(){
+	
+	// This method is used to display the Gambling Game starting
+	static void display() {
+		System.out.println("Welcome to the Gambling Game."); 
+	}
+	
+	// This method returns true(Win) ewhen probability is less than 0.5 else return false(loose)..	
+	static boolean winOrLoss() {
 		
-		numberOfGames++;
-		if(Math.random()<0.5){
-			System.out.println("You won!!");
-			Stake+=Bet;
+		if(Math.random() < 0.5) {
 			return true;
-		}else{
-			System.out.println("You Loose!!");
-			Stake-=Bet;
+		}else {
 			return false;
 		}
 	}
+	
+	//This method is for calculate the stake till the gambler won or lost 50% of the stake
+	static void playGame() {
+	
+		game();
+		
+		final int MaxWinAmout = stake + (stake * 50 / 100);
+		final int MaxLoseAmount = stake - (stake * 50 / 100);
+
+		while(stake>= MaxLoseAmount && stake<=MaxWinAmout) {
+			game();
+		}
+		
+	}
+	
+	// This method returns the value in int and is used for calculating the stake for every play on the basis of win or loose by invoking the winOrLoss method..
+	static int  game() {
+		
+		numberOfGames++;
+		
+		 if(winOrLoss()) {
+			stake+=Bet;
+			System.out.println("You won!! \n The new Stake value is "+stake);
+		}else {
+			stake-=Bet;
+			System.out.println("You Lose!! \n The new Stake value is" +stake);
+		}
+		 
+		 return stake;
+	}
+	
 }
