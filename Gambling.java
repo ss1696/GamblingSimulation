32fc336ebbb3f054078a3d70f2b50f37cec3a995
shashank srivastava <shashank.1696@gmail.com>
2020-07-31 01:46:08 +0530

    Added: Invoked Methods to calculate the stake till the gambler either looses or wins the 50% of the stake

--- Gambling.java
+++ Gambling.java
@@ -1,58 +1,37 @@
 public class Gambling{
 	
 	// Initializing the Varible ...
-	static int stake = 100;
-	final static int Bet = 1;
-	static int numberOfGames=0;
-	
-	public static void main(String[] args){
-		display();
-		playGame();
-	}
-	
-	// This method is used to display the Gambling Game starting
-	static void display() {
-		System.out.println("Welcome to the Gambling Game."); 
-	}
-	
-	// This method returns true(Win) ewhen probability is less than 0.5 else return false(loose)..	
-	static boolean winOrLoss() {
-		
-		if(Math.random() < 0.5) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
-	//This method is for calculate the stake till the gambler won or lost 50% of the stake
-	static void playGame() {
-	
-		game();
-		
-		final int MaxWinAmout = stake + (stake * 50 / 100);
-		final int MaxLoseAmount = stake - (stake * 50 / 100);
+	final int Stake = 100;
+	final int Bet = 1;
+    	
+	int numberOfGames,cash;
 
-		while(stake>= MaxLoseAmount && stake<=MaxWinAmout) {
-			game();
-		}
-		
-	}
-	
-	// This method returns the value in int and is used for calculating the stake for every play on the basis of win or loose by invoking the winOrLoss method..
-	static int  game() {
-		
-		numberOfGames++;
-		
-		 if(winOrLoss()) {
-			stake+=Bet;
-			System.out.println("You won!! \n The new Stake value is "+stake);
-		}else {
-			stake-=Bet;
-			System.out.println("You Lose!! \n The new Stake value is" +stake);
-		}
-		 
-		 return stake;
-	}
-	
+	public static void main(String[] args){
+    		System.out.println("Welcome to the Gambling Game.");
+    		GamblingPlay gambler = new GamblingPlay();
+            	gambler.playGame();
+    	}
+
+    	// This method returns the value in int and is used for calculating the cash for every play on the basis of win or loose by invoking the winOrLoss method..
+    	void game() {
+             if(Math.random() < 0.5) {
+                    cash+=Bet;
+            		System.out.println("Won the game \n"+cash+" the value of Stake");
+            }else {
+                    cash-=Bet;
+            		System.out.println("Loss the game \n"+cash+" the value of Stake");
+            }
+    	}
+    
+    	//This method is for calculate the cash till the gambler won or lost 50% of the cash
+    	void playGame() {
+            final int MaxWinAmout = Stake + (Stake * 50 / 100);
+            final int MaxLoseAmount = Stake - (Stake * 50 / 100);
+            cash = Stake;
+            numberOfGames = 0;
+            while(cash>MaxLoseAmount && cash<MaxWinAmout) {
+            	numberOfGames++;
+                game();
+            }
+    	}	
 }
