6f76e127a6e31c8f506b47dc0e456c5e818b287d
shashank srivastava <shashank.1696@gmail.com>
2020-07-31 02:11:24 +0530

    Refactor: Invoked method to calculate gambler's total wining & loosing fot each days after 20th day of month..

--- Gambling.java
+++ Gambling.java
@@ -1,37 +1,54 @@
+
 public class Gambling{
 	
-	// Initializing the Varible ...
+	// Initializing the Constant & Variables...
 	final int Stake = 100;
 	final int Bet = 1;
-    	
-	int numberOfGames,cash;
-
-	public static void main(String[] args){
-    		System.out.println("Welcome to the Gambling Game.");
-    		GamblingPlay gambler = new GamblingPlay();
-            	gambler.playGame();
-    	}
-
-    	// This method returns the value in int and is used for calculating the cash for every play on the basis of win or loose by invoking the winOrLoss method..
-    	void game() {
-             if(Math.random() < 0.5) {
-                    cash+=Bet;
-            		System.out.println("Won the game \n"+cash+" the value of Stake");
-            }else {
-                    cash-=Bet;
-            		System.out.println("Loss the game \n"+cash+" the value of Stake");
-            }
-    	}
-    
-    	//This method is for calculate the cash till the gambler won or lost 50% of the cash
-    	void playGame() {
-            final int MaxWinAmout = Stake + (Stake * 50 / 100);
-            final int MaxLoseAmount = Stake - (Stake * 50 / 100);
-            cash = Stake;
-            numberOfGames = 0;
-            while(cash>MaxLoseAmount && cash<MaxWinAmout) {
-            	numberOfGames++;
-                game();
-            }
-    	}	
+	final int NoOfDayInMonth = 30;
+	int numberOfGames, dayWinAmt, dayLossAmt,cash;
+	int totalcashAmtWon, totalcashAmtLost;
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Gambling Game."); 
+		Gambling gambler = new Gambling();
+		gambler.monthPlay();
+	}
+	
+	//This method is used for calculating the cash for every play on the basis of win or loose and also updates the daily win & loose amount..  
+	private void  playGame() {
+		if(Math.random() < 0.5) {
+			cash = cash + Bet;
+			dayWinAmt++;
+		} else {
+			cash = cash - Bet;
+			dayLossAmt++;
+		}
+	}
+	
+	//This method is used to display total amount won & loose for each days after 20th day in a month..
+	private void monthPlay() {
+		int MaxWinAmout = Stake + (Stake*50/100);
+		int MaxLoseAmount = Stake -(Stake*50/100);
+		
+		for(int i=1; i<=NoOfDayInMonth; i++) {
+			cash = Stake;
+			numberOfGames = 0;dayWinAmt = 0;dayLossAmt = 0;
+			while(cash>MaxLoseAmount && cash<MaxWinAmout) {
+				numberOfGames++;
+				playGame();
+			}
+			
+			if(dayWinAmt > dayLossAmt) {
+				totalcashAmtWon = totalcashAmtWon + (dayWinAmt - dayLossAmt);
+			}
+			else {
+				totalcashAmtLost = totalcashAmtLost + (dayLossAmt - dayWinAmt);
+			}
+			if(i>=20) {
+				System.out.println("Total Amount Won at day"+i+" "+totalcashAmtWon);
+				System.out.println("Total Amount Lost at day"+i+" "+totalcashAmtLost);
+			}	
+		}	
+	}
+	
 }
