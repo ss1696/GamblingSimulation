f500edf818d7488ab5edc3ed84da40f548c66ffa
shashank srivastava <shashank.1696@gmail.com>
2020-07-31 05:50:15 +0530

    Refactor: Method to get the gambler's luckiest day and unluckiest day on the basis of his win and loose in a month

--- Gambling.java
+++ Gambling.java
@@ -1,4 +1,6 @@
-
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map.Entry;;
 public class Gambling{
 	
 	// Initializing the Constants & Variables...
@@ -7,6 +9,9 @@ public class Gambling{
 	final int NoOfDayInMonth = 30;
 	int numberOfGames, dayWinAmt, dayLossAmt,cash;
 	int totalcashAmtWon, totalcashAmtLost;
+	float winPercentage, loosePercentage;
+	HashMap<Integer,Float> win = new HashMap<Integer,Float>();
+	HashMap<Integer,Float> loss = new HashMap<Integer,Float>();
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Gambling Game."); 
@@ -39,15 +44,33 @@ public class Gambling{
 			}
 			
 			if(dayWinAmt > dayLossAmt) {
+				winPercentage = ( dayWinAmt * 100 / numberOfGames );
+				win.put(i, winPercentage);
 				totalcashAmtWon = totalcashAmtWon + (dayWinAmt - dayLossAmt);
 				System.out.println("Total Amount Won at day"+i+" "+totalcashAmtWon);
 			}
 			else {
+				loosePercentage = ( dayLossAmt * 100 / numberOfGames );
+				loss.put(i, loosePercentage);
 				totalcashAmtLost = totalcashAmtLost + (dayLossAmt - dayWinAmt);
 				System.out.println("Total Amount Lost at day"+i+" "+totalcashAmtLost);
 			}
 			i++;
 		}	
+		
+		// This will return max value in the Hashmap through which we get the gambler's luckiest and Unluckiest Day
+		float maxWinDay=(Collections.max(win.values()));  
+		for (Entry<Integer, Float> entry : win.entrySet()) { 
+			if (entry.getValue()==maxWinDay) {
+				System.out.println("The Luckiest Day of the Gambler is "+entry.getKey());     
+			}
+		}
+		float maxLossDay=(Collections.max(loss.values()));  
+		for (Entry<Integer, Float> entry : loss.entrySet()) { 
+			if (entry.getValue()==maxLossDay) {
+				System.out.println("The UnLuckiest Day of the Gambler is "+entry.getKey());     
+			}
+		}
 	}
 	
 }
