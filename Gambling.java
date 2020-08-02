47e6257d32441b089d32aa521d20dcd1cecbbdc9
shashank srivastava <shashank.1696@gmail.com>
2020-08-02 21:30:30 +0530

    Added: Invoked Method to calculate the Stake till the gambler either looses or wins the 50% of Stake

--- Gambling.java
+++ Gambling.java
@@ -1,35 +1,37 @@
 public class Gambling{
 	
-	// Initializing the Varible ...
-	final int Stake = 100;
-	final int Bet = 1;
-    	
-	int numberOfGames,cash;
+	// Initializing Constant and the Varible ...
+	final int STAKE = 100;
+	final int BET = 1;
+    	int cash;
+	int goal;
+	int numberOfGames;
 
 	public static void main(String[] args){
     		System.out.println("Welcome to the Gambling Game.");
-    		GamblingPlay gambler = new GamblingPlay();
+    		Gambling gambler = new Gambling();
             	gambler.playGame();
     	}
 
     	// This method returns the value in int and is used for calculating the cash for every play on the basis of win or loose by invoking the winOrLoss method..
     	void game() {
              if(Math.random() < 0.5) {
-                    cash+=Bet;
+                    cash += BET;
             		System.out.println("Won the game \n"+cash+" the value of Stake");
             }else {
-                    cash-=Bet;
+                    cash -= BET;
             		System.out.println("Loss the game \n"+cash+" the value of Stake");
             }
     	}
     
     	//This method is for calculate the cash till the gambler won or lost 50% of the cash
     	void playGame() {
-            final int MaxWinAmout = Stake + (Stake * 50 / 100);
-            final int MaxLoseAmount = Stake - (Stake * 50 / 100);
-            cash = Stake;
+	    goal =  (STAKE * 50 / 100);
+            final int MaxWinAmout = STAKE + goal;
+            final int MaxLoseAmount = STAKE - goal;
+            cash = STAKE;
             numberOfGames = 0;
-            while(cash>MaxLoseAmount && cash<MaxWinAmout) {
+            while(cash > MaxLoseAmount && cash < MaxWinAmout) {
             	numberOfGames++;
                 game();
             }
