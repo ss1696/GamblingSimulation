66b07617ec44c3b64350120fb6370d1aa306e4ce
shashank srivastava <shashank.1696@gmail.com>
2020-08-02 21:40:00 +0530

    REFACTOR: Method to calculate gamblers total wining and lossing for each day after 20th day of month

--- Gambling.java
+++ Gambling.java
@@ -2,11 +2,16 @@
 public class Gambling{
 	
 	// Initializing the Constants & Variables...
-	final int Stake = 100;
-	final int Bet = 1;
-	final int NoOfDayInMonth = 30;
-	int numberOfGames, dayWinAmt, dayLossAmt,cash;
-	int totalcashAmtWon, totalcashAmtLost;
+	final int STAKE = 100;
+	final int BET = 1;
+	final int NO_OF_DAYS_MONTH = 30;
+	int numberOfGames;
+	int goal;
+	int dayWinAmt;
+	int dayLossAmt;
+	int cash;
+	int totalcashAmtWon;
+	int totalcashAmtLost;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Gambling Game."); 
@@ -17,34 +22,37 @@ public class Gambling{
 	//This method is used for calculating the cash for every play on the basis of win or loose and also updates the daily win & loose amount..  
 	private void  playGame() {
 		if(Math.random() < 0.5) {
-			cash = cash + Bet;
+			cash = cash + BET;
 			dayWinAmt++;
 		} else {
-			cash = cash - Bet;
+			cash = cash - BET;
 			dayLossAmt++;
 		}
 	}
 	
 	//This method is used to display total amount won & loose for each days after 20th day in a month..
 	private void monthPlay() {
-		int MaxWinAmout = Stake + (Stake*50/100);
-		int MaxLoseAmount = Stake -(Stake*50/100);
+		goal = (STAKE * 50 / 100);
+		int MaxWinAmout = STAKE + goal;
+		int MaxLoseAmount = STAKE - goal;
 		
-		for(int i=1; i<=NoOfDayInMonth; i++) {
-			cash = Stake;
-			numberOfGames = 0;dayWinAmt = 0;dayLossAmt = 0;
-			while(cash>MaxLoseAmount && cash<MaxWinAmout) {
+		for(int i=1; i<=NO_OF_DAYS_MONTH; i++) {
+			cash = STAKE;
+			numberOfGames = 0;
+			dayWinAmt = 0;
+			dayLossAmt = 0;
+			while(cash > MaxLoseAmount && cash < MaxWinAmout) {
 				numberOfGames++;
 				playGame();
 			}
 			
 			if(dayWinAmt > dayLossAmt) {
 				totalcashAmtWon = totalcashAmtWon + (dayWinAmt - dayLossAmt);
-			}
-			else {
+			} else {
 				totalcashAmtLost = totalcashAmtLost + (dayLossAmt - dayWinAmt);
 			}
-			if(i>=20) {
+
+			if(i >= 20) {
 				System.out.println("Total Amount Won at day"+i+" "+totalcashAmtWon);
 				System.out.println("Total Amount Lost at day"+i+" "+totalcashAmtLost);
 			}	
